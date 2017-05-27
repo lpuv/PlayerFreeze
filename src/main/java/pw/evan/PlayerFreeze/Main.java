b1ff68cddff2927b239328cae610c7576f5c9cb3
Evan <evan.straw99@gmail.com>
2017-05-26 21:47:52 -0700

    Created main class and plugin.yml

---
+++ src/main/java/pw/evan/PlayerFreeze/Main.java
@@ -0,0 +1,14 @@
+package pw.evan.PlayerFreeze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
