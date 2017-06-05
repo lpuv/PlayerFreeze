48d74a520db0b26c8060cd4772cee0c2783c9bdd
Evan <evan.straw99@gmail.com>
2017-06-04 23:34:43 -0700

    Added startup code for when the plugin enables

--- src/main/java/pw/evan/PlayerFreeze/Main.java
+++ src/main/java/pw/evan/PlayerFreeze/Main.java
@@ -2,7 +2,14 @@ package pw.evan.PlayerFreeze;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import pw.evan.PlayerFreeze.command.Freeze;
+import pw.evan.PlayerFreeze.command.FreezeAll;
+import pw.evan.PlayerFreeze.command.Unfreeze;
+import pw.evan.PlayerFreeze.command.UnfreezeAll;
+import pw.evan.PlayerFreeze.listener.PlayerBlockListener;
+import pw.evan.PlayerFreeze.listener.PlayerMoveListener;
 import pw.evan.PlayerFreeze.manager.UserManager;
+import pw.evan.PlayerFreeze.scheduler.TempFreezeChecker;
 import pw.evan.PlayerFreeze.util.ChatUtil;
 
 public class Main extends JavaPlugin
@@ -19,12 +26,23 @@ public class Main extends JavaPlugin
 
     public void onEnable()
     {
-        
+        ChatUtil.console(this,getPluginPrefix()+"&aVersion &bv"+getDescription().getVersion()+" &aenabling...");
         UserManager.get(this); //create a UserManager
+
+        getCommand("freeze").setExecutor(new Freeze(this));
+        getCommand("freezeall").setExecutor(new FreezeAll(this));
+        getCommand("unfreeze").setExecutor(new Unfreeze(this));
+        getCommand("unfreezeall").setExecutor(new UnfreezeAll(this));
+
+        getServer().getPluginManager().registerEvents(new PlayerBlockListener(),this);
+        getServer().getPluginManager().registerEvents(new PlayerMoveListener(),this);
+
+        new TempFreezeChecker(this).runTaskTimer(this,0,20);
+        ChatUtil.console(this,getPluginPrefix()+"&aVersion &bv"+getDescription().getVersion()+" &aenabled!");
     }
 
     public void onDisable()
     {
-
+        ChatUtil.console(this,getPluginPrefix()+"&cVersion &bv"+getDescription().getVersion()+" &cdisabled!");
     }
 }
--- src/main/java/pw/evan/PlayerFreeze/util/ChatUtil.java
+++ src/main/java/pw/evan/PlayerFreeze/util/ChatUtil.java
@@ -1,10 +1,16 @@
 package pw.evan.PlayerFreeze.util;
 
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import pw.evan.PlayerFreeze.Main;
 
 public class ChatUtil
 {
     public static String colorize(String message){
         return ChatColor.translateAlternateColorCodes('&',message);
+    }
+
+    public static void console(Main plugin, String message){
+        plugin.getServer().getConsoleSender().sendMessage(colorize(message));
     }
 }
